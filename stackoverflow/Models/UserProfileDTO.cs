be89b1062ca1da9ddc17f2c74c2e42e2de4222d3
Mohammad Ali Taqvazadeh <m.a.taghvazadeh@gmail.com>
2018-09-26 19:38:10 +0330

    Add User Profile DTO

---
+++ stackoverflow/Models/UserProfileDTO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace stackoverflow.Models
+{
+    public class UserProfileDTO
+    {
+        public UserProfileDTO()
+        {
+            Answers = new HashSet<Answer>();
+            Comments = new HashSet<Comment>();
+            Questions = new HashSet<Question>();
+        }
+
+        public int Id { get; }
+
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        public DateTime RegisterDate { get; }
+
+        public DateTime? LastLogin { get; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Answer> Answers { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Comment> Comments { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Question> Questions { get; set; }
+    }
+}
